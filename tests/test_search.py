cd1c10a52602179cb5a6eb0174223e29d4aa47b6
douagz <122457822+douagz@users.noreply.github.com>
2024-02-22 22:02:57 +0800

    Update test_search.py

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,31 +1,15 @@
 import pytest
-from playwright.sync_api import Page, expect
-from pages.search import BaiduSearchPage
-from pages.result import BaiduResultPage
-from common.load_baidu_search_data import load_baidu_search_data
+from baidu_search.common.load_baidu_search_data import load_baidu_search_data
 
-data=load_baidu_search_data()
 
-@pytest.mark.parametrize('phrase',data['phrases'])
-def test_basic_baidu_search(page,
-                            search_page,
-                            result_page,
-                            phrase):
+class TestBaiduSearch:
+    baidu_search_data = load_baidu_search_data()
 
-    # Given the Baidu home page is displayed
-    search_page.load()
-
-    # When the user searches for a phrase
-    try:
-        search_page.search(phrase['phrase'])
-    except Exception:
-        pytest.fail("Case Failed")
-
-    # Then the search result query is the phrase
-    expect(result_page.search_input).to_have_value(phrase['phrase'])
-
-    # And the search result links pertain to the phrase
-    assert result_page.result_link_titles_contain_phrase(phrase['phrase'])
-
-    # And the search result title contains the phrase
-    expect(page).to_have_title(phrase['expected_title'])
+    @pytest.mark.parametrize("search_data", baidu_search_data)
+    def test_baidu_search_01(self, baidu_search_page, baidu_result_page, search_data):
+        baidu_search_page.load()
+      
+        baidu_search_page.search(search_data['phrase'])
+      
+        assert baidu_result_page.get_title() == search_data['expected_title']
+        assert baidu_result_page.result_link_titles_contain_phrase(search_data['phrase'])
