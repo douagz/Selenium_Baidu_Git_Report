7f8e44d97896a6331696fe0b25cbf88a1ad877a2
douagz <122457822+douagz@users.noreply.github.com>
2024-02-22 22:20:14 +0800

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,15 +9,8 @@ class Config:
     root_dir = os.path.split(os.path.split(__file__)[0])[0]
     test_cases_dir = root_dir + os.path.sep + "tests"
     test_data_dir = root_dir + os.path.sep + "data"
-    test_report_dir = root_dir + os.path.sep + "report" + os.path.sep + "AllureReport"
-    test_result_dir = root_dir + os.path.sep + "report" + os.path.sep + "AllureResult"
-    test_screenshot_dir = root_dir + os.path.sep + "report" + os.path.sep + "Screenshot"
-    logs_dir = root_dir + os.path.sep + "Logs"
 
-    # 权限认证目录
-    auth_dir = root_dir + os.path.sep + "auth"
-
-    # 测试文件
+    # 测试数据文件
     baidu_search_data_file = root_dir + os.path.sep + "data/baidu_search.yml"
 
 
